2fcfaad8e04afb1bb54fa4979e470316e611b161
Jialong ZHANG <JZhang182@slb.com>
2020-11-17 14:38:38 +0800

    feat: add input validation

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,15 @@
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
+  public render(start: number, end: number): string {
+    const errMessage = this.validateInput(start, end)
+    if (errMessage !== 'OK') return errMessage
+
+
     return '1*1=1'
+  }
+
+  private validateInput(start: number, end: number): string {
+    if (start > end) return ''
+    if (start < 1 || start > 10 || end < 1 || end > 10) return 'Error: Input numbers are out of range.'
+    return 'OK'
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,4 +1,4 @@
-import {MultiplicationTable} from '../src/MultiplicationTable'
+import { MultiplicationTable } from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
@@ -12,5 +12,29 @@ describe('Multiplication Table', () => {
 
     //then
     expect(rendered).toBe('1*1=1')
+  })
+  it('should return empty string of input (2, 1)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
+  it('should return out of range error message of input (0, 1)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 0
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('Error: Input numbers are out of range.')
   })
 })
